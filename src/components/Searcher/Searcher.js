12ef09ee19542e7f17a4e1af74b0c6b716fb3ce6
juan.palacios@sryas.com <jcmaster_97@hotmail.com>
2022-11-25 10:05:09 -0600

    20221125 changes

--- src/components/Searcher/Searcher.js
+++ src/components/Searcher/Searcher.js
@@ -19,8 +19,8 @@ class Searcher extends Component {
             <nav className="navbar navbar-expand-lg navbar-black bg-secondary">
                 <div className="container-fluid">
                     <h1 className="text-primary" href="#">Image searcher</h1>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <form className="d-flex" onSubmit={this.handleSubmit}>
                         <div className="row justify-content-center">
